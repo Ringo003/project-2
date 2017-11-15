87968a09db18df8d1099ba43225db1f0a9881f4a
Fu Ck U <d.foxultra@gmail.com>
2017-11-14 23:37:48 -0600

    filled in null values and created smaller tables

--- price_schema.sql
+++ price_schema.sql
@@ -1,9 +1,10 @@
 create database exoplanet_db;
-	use exoplanet_db;
-	create table exosale(
-		rowid int(11) not null primary key,
-        st_dist decimal(11,11),
+        use exoplanet_db;
+        create table exosale(
+        rowid int(11) not null primary key,
+        st_dist float(15) not null default 0.0,
         pl_name varchar(255),
-        pl_masseerr1 decimal(11,11),
-        st_actlx decimal(11,11)
-		);
+        pl_masseerr1 float(15) not null default 0.0,
+        st_actlx float(15) not null default 0.0,
+        sold tinyint(1) not null default 0.0
+        );
--- prod-info.sql
+++ prod-info.sql
@@ -1,20 +1,18 @@
-create database exoplanet_db;
-	use exoplanet_db;
-	create table product_info(
-		rowid int(11) not null primary key,
+create table product_info(
+        rowid int(11) not null primary key,
         pl_hostname varchar(255) not null,
         pl_pnum int(11) not null,
-        pl_orbper decimal(11,11),
-        pl_denserr1 decimal(11,11),
-        st_dist decimal(11,11),
-        st_teff decimal(11,11),
-        st_mass decimal(11,11),
+        pl_orbper FLOAT(15) not null default 0.0,
+        pl_denserr1 FLOAT(15) not null default 0.0,
+        st_dist FLOAT(15) not null default 0.0,
+        st_teff FLOAT(15) not null default 0.0,
+        st_mass FLOAT(15) not null default 0.0,
         rowupdate varchar(255),
         pl_name varchar(255),
-        pl_eqt int(11),
-        pl_masseerr1 decimal(11,11),
-		pl_ratdorerr1 decimal(11,11),
-        st_glon decimal(11,11),
-        st_glat decimal(11,11),
-        st_actlx decimal(11,11)
-		);
+        pl_eqt int(11) default 0.0,
+        pl_masseerr1 FLOAT(15) not null default 0.0,
+        pl_ratdorerr1 FLOAT(15) not null default 0.0,
+        st_glon FLOAT(15) not null default 0.0,
+        st_glat FLOAT(15) not null default 0.0,
+        st_actlx FLOAT(15) not null default 0.0
+        );
